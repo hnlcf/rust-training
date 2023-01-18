d33500e437f200690d6312150f5e21c2674d21b9
Changfeng <louchangfeng@outlook.com>
2023-01-18 18:37:20 +0800

    chore(proc_macros): add comment

--- proc_macros/level1/src/lib.rs
+++ proc_macros/level1/src/lib.rs
@@ -4,6 +4,7 @@ use crate::json_schema::get_token_literal;
 use json_schema::StructsTemplate;
 use proc_macro::TokenStream;
 
+/// Map a json schema to correponding Rust structures
 #[proc_macro]
 pub fn generate(input: TokenStream) -> TokenStream {
     let filename = get_token_literal(input).unwrap();
